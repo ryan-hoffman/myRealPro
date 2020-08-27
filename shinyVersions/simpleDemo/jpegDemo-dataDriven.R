a9a9d03aa1cd1b6244916f6ad49d5b9e7dd444a6
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-08-27 09:28:11 -0700

    pictures and their urls removed from app, no in separate yaml file

---
+++ shinyVersions/simpleDemo/jpegDemo-dataDriven.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(yaml)
+pictures <- yaml.load(readLines("pictures.yaml"))
+
+
+ui = fluidPage(
+   titlePanel("Picture Choosing Demo"),
+   sidebarLayout(
+     sidebarPanel(
+         selectInput(inputId="pictureSelector",
+                     label="Select Picture",
+                     choices=c("no picture", names(pictures)),
+                     selected=NULL)
+         ),
+      mainPanel(
+        uiOutput("pictureOutput")
+        )
+    ) # sidebarPanel
+  ) # fluidPage
+
+server = function(input, output) {
+    output$pictureOutput <- renderUI({
+      pictureName <- input$pictureSelector
+      if(pictureName != "no picture"){
+         pictureURL <- pictures[[pictureName]]
+         tags$img(src=pictureURL, width=300)
+         } # if
+    }) # renderUI
+  } # server
+
+runApp(shinyApp(ui, server), port=8888)
