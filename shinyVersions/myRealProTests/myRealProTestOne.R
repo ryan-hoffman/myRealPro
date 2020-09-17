2703e3aac087cb8ab092895addabbb5e32038f4d
Ryan Hoffman <ryan@ryanhoffmanmusic.com>
2020-09-17 10:32:37 -0700

    additions to mRPT, mRPTyaml, tunes, and mRPTyamlexample

--- shinyVersions/myRealProTests/myRealProTestOne.R
+++ shinyVersions/myRealProTests/myRealProTestOne.R
@@ -1,18 +1,31 @@
 library(shiny)
 addResourcePath("www", "www");
 
-celtic_tunes = list(
+celtic_reels = list(
   "Congress Reel" = "www/congress.jpg",
   "Earl's Chair" = "www/earlschair.jpg",
   "Mulqueen's Reel" = "www/mulqueens.jpg",
   "Traveler's" = "www/travelers.jpg"
 )
 
-jazz_tunes = list(
+celtic_jigs = list(
+  "Blarney Pilgrim" = "www/blarney_pilgrim.jpg",
+  "Champion" = "www/champion.jpg",
+  "Geese in the Bog" = "www/geese_in_the_bog.jpg",
+  "Leitrim Fancy" = "www/leitrim_fancy.jpg"
+)
+
+swing_tunes = list(
   "After You've Gone" = "www/after_youve_gone.jpg",
   "Ain't Misbehavin'" = "www/aint_misbehavin.jpg",
   "All of Me" = "www/all_of_me.jpg",
   "All the Things You Are" = "www/all_the_things_you_are.jpg"
+)
+
+latin_tunes = list(
+  "Gentle Rain" = "www/gentle_rain.jpg",
+  "Mambo Inn" = "www/mambo_inn.jpg",
+  "Manha de Carnival" = "www/manha_de_carnival.jpg"
 )
 
 ui <- fluidPage(
@@ -20,39 +33,69 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput(
-        inputId = "celticTuneSelector",
-        label = "Celtic Tunes",
-        choices = c("no selection", names(celtic_tunes)),
+        inputId = "reelSelector",
+        label = "Reels",
+        choices = c("no selection", names(celtic_reels)),
         selected = NULL),
 
       selectInput(
-        inputId = "jazzTuneSelector",
-        label = "Jazz Tunes",
-        choices = c("no selection", names(jazz_tunes)),
+        inputId = "jigSelector",
+        label = "Jigs",
+        choices = c("no selection", names(celtic_jigs)),
+        selected = NULL),
+      
+      selectInput(
+        inputId = "swingSelector",
+        label = "Swing",
+        choices = c("no selection", names(swing_tunes)),
+        selected = NULL),
+      
+      selectInput(
+        inputId = "latinSelector",
+        label = "Latin",
+        choices = c("no selection", names(latin_tunes)),
         selected = NULL)
     ), # sbp
 
     mainPanel(
-      uiOutput("celticTuneOutput"),
-      uiOutput("jazzTuneOutput")
+      uiOutput("reelOutput"),
+      uiOutput("jigOutput"),
+      uiOutput("swingOutput"),
+      uiOutput("latinOutput")
     ) # m
   ) # sbl
 ) # fp
 
 server <- function(input,output){
-
-  output$celticTuneOutput <- renderUI({
-    tuneName <- input$celticTuneSelector
+  
+  output$reelOutput <- renderUI({
+    tuneName <- input$reelSelector
     if(tuneName != "no selection"){
-      tuneFile <- celtic_tunes[[tuneName]]
+      tuneFile <- celtic_reels[[tuneName]]
       tags$img(src=tuneFile, width=500)
     }
   })
 
-  output$jazzTuneOutput <- renderUI({
-    tuneName <- input$jazzTuneSelector
+  output$jigOutput <- renderUI({
+    tuneName <- input$jigSelector
     if(tuneName != "no selection"){
-      tuneFile <- jazz_tunes[[tuneName]]
+      tuneFile <- celtic_jigs[[tuneName]]
+      tags$img(src=tuneFile, width=500)
+    }
+  })
+  
+  output$swingOutput <- renderUI({
+    tuneName <- input$swingSelector
+    if(tuneName != "no selection"){
+      tuneFile <- swing_tunes[[tuneName]]
+      tags$img(src=tuneFile, width=500)
+    }
+  })
+  
+  output$latinOutput <- renderUI({
+    tuneName <- input$latinSelector
+    if(tuneName != "no selection"){
+      tuneFile <- latin_tunes[[tuneName]]
       tags$img(src=tuneFile, width=500)
     }
   })
--- shinyVersions/myRealProTests/myRealProTestOne_yaml.R
+++ shinyVersions/myRealProTests/myRealProTestOne_yaml.R
@@ -8,42 +8,73 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput(
-        inputId = "celticTuneSelector",
-        label = "Celtic Tunes",
+        inputId = "reelSelector",
+        label = "Reels",
         choices = c("no selection", names(tunes)),
         selected = NULL),
       
       selectInput(
-        inputId = "jazzTuneSelector",
-        label = "Jazz Tunes",
+        inputId = "jigSelector",
+        label = "Jigs",
+        choices = c("no selection", names(tunes)),
+        selected = NULL),
+      
+      selectInput(
+        inputId = "swingSelector",
+        label = "Swing",
+        choices = c("no selection", names(tunes)),
+        selected = NULL),
+      
+      selectInput(
+        inputId = "latinSelector",
+        label = "Latin",
         choices = c("no selection", names(tunes)),
         selected = NULL)
     ), # sbp
     
     mainPanel(
-      uiOutput("celticTuneOutput"),
-      uiOutput("jazzTuneOutput")
+      uiOutput("reelOutput"),
+      uiOutput("jigOutput"),
+      uiOutput("swingOutput"),
+      uiOutput("latinOutput")
     ) # m
   ) # sbl
 ) # fp
 
 server <- function(input,output){
   
-  output$celticTuneOutput <- renderUI({
-    tuneName <- input$celticTuneSelector
+  output$reelOutput <- renderUI({
+    tuneName <- input$reelSelector
     if(tuneName != "no selection"){
       tuneFile <- tunes[[tuneName]]
       tags$img(src=tuneFile, width=500)
     }
   })
   
-  output$jazzTuneOutput <- renderUI({
-    tuneName <- input$jazzTuneSelector
+  output$jigOutput <- renderUI({
+    tuneName <- input$jigSelector
     if(tuneName != "no selection"){
       tuneFile <- tunes[[tuneName]]
       tags$img(src=tuneFile, width=500)
-    }  
+    }
+  })
+  
+  output$swingOutput <- renderUI({
+    tuneName <- input$swingSelector
+    if(tuneName != "no selection"){
+      tuneFile <- tunes[[tuneName]]
+      tags$img(src=tuneFile, width=500)
+    }
+  })
+  
+  output$latinOutput <- renderUI({
+    tuneName <- input$latinSelector
+    if(tuneName != "no selection"){
+      tuneFile <- tunes[[tuneName]]
+      tags$img(src=tuneFile, width=500)
+    }
   })
 }
+
 
 shinyApp(ui = ui, server = server)
